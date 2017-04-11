aca834113218a6cf13e7e2e2907106dc81d350eb
Egor <youcankeepsilence@gmail.com>
2017-04-11 23:25:49 +0300

    added add and erase func, need to rework it at tiles

--- glwidget.cpp
+++ glwidget.cpp
@@ -2,23 +2,72 @@
 
 GlWidget::GlWidget(QWidget *parent)
     : QGLWidget(parent) ,
-    xleft(-20),
-    xright(20),
-    ytop(20),
-    ybot(0),
-    x(-2.5),
-    y(2),
-    xstep(0.5),
-    ystep(1),
-    rectWidth(5)
-{
+    xleft(0),
+    xright(40),
+    ytop(40),
+    ybot(0)
 
+{
+    xstep = (xright - xleft)/X_TILE_COUNT;
+    ystep = (ytop - ybot)/Y_TILE_COUNT;
+    sizeOfSide = xstep;
+    x=xleft;
+    y=ybot+ystep;
+    for(int i = 0 ; i < X_TILE_COUNT ; i++)
+    {
+        for(int j = 0; j < Y_TILE_COUNT ; j++)
+        {
+            tiles[i][j]=0;
+        }
+    }
 }
 
 GlWidget::~GlWidget()
 {
 
 }
+void
+GlWidget::eraseRectangle(float x, float y)
+{
+    if(!rectangles.isEmpty())
+    {
+        foreach (myFigure::Rectnagle* rec, rectangles)
+        {
+            if(rec->x == x && rec->y == y)
+            {
+                rectangles.removeAt(rectangles.indexOf(rec));
+                delete rec;
+                return;
+            }
+        }
+    }
+}
+
+void
+GlWidget::addRectangle(float x, float y)//, float size)
+{
+    if(!rectangles.isEmpty())
+    {
+        foreach (myFigure::Rectnagle* rec, rectangles)
+        {
+            if(rec->x == x && rec->y == y)
+            {
+                qDebug()<<"already exist";
+                return;
+            }
+        }
+    }
+    myFigure::Rectnagle* rec = new myFigure::Rectnagle;
+    rec->x = x;
+    rec->y = y;
+    rec->size = xstep; // квадрату плевать
+    rectangles.append(rec);
+    int tilex = floor(x / xstep);
+    int tiley = floor((y - rec->size) / ystep);
+    tiles[tilex][tiley] = 1;
+//    qDebug()<<"added x "<<x<<" tilex "<<tilex<<" y "<<y<< " tiley "<<tiley;
+}
+
 void
 GlWidget::initializeGL()
 {
@@ -40,7 +89,15 @@ GlWidget::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     qglColor(Qt::darkCyan);
-    glRectf(x,y,x+rectWidth,y-rectWidth);
+
+    foreach (myFigure::Rectnagle* rec, rectangles)
+    {
+        glRectf(rec->x,rec->y,rec->x+sizeOfSide,rec->y-sizeOfSide);
+    }
+    qglColor(Qt::darkMagenta);
+    glRectf(x,y,x+sizeOfSide,y-sizeOfSide);
+
+
 }
 void
 GlWidget::keyPressEvent(QKeyEvent *event)
@@ -52,7 +109,7 @@ GlWidget::keyPressEvent(QKeyEvent *event)
             x -= xstep;
         break;
     case Qt::Key_Right:
-        if(x+rectWidth < xright)
+        if(x+sizeOfSide < xright)
             x += xstep;
         break;
     case Qt::Key_Up:
@@ -60,10 +117,15 @@ GlWidget::keyPressEvent(QKeyEvent *event)
             y+= ystep;
         break;
     case Qt::Key_Down:
-        if( y - rectWidth > ybot)
+        if( y - sizeOfSide > ybot)
             y -= ystep;
         break;
-    case Qt::Key_R:
+    case Qt::Key_D:
+//        qDebug()<<"pressed";
+        addRectangle(x,y);
+        break;
+    case Qt::Key_E:
+        eraseRectangle(x,y);
         break;
 
     }
