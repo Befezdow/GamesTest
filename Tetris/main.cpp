8ffd2cd6dbea30c0d8397a5771811dfe53e94f67
Egor <youcankeepsilence@gmail.com>
2017-04-22 00:06:32 +0300

    fixed memory

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -6,19 +6,19 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     int screenWidth=QApplication::desktop()->width();
-    GameArea gameArea(screenWidth/45,10,15);
-    Scoretable scores(screenWidth/68);
-    QObject::connect(&gameArea,SIGNAL(scoreChanged(int)),&scores,SLOT(changeScore(int)));
-    QObject::connect(&gameArea,SIGNAL(throwNextFigure(int,QColor)),&scores,SLOT(setNextFigure(int,QColor)));
-    QWidget wgt;
-    QHBoxLayout lay;
-    lay.addWidget(&gameArea);
-    lay.addWidget(&scores);
-    wgt.setLayout(&lay);
-    wgt.setFocusProxy(&gameArea);
-    scores.setFocusProxy(&gameArea);
-    gameArea.setFocus();
-    wgt.show();
-    gameArea.start();
+    GameArea * gameArea=new GameArea(screenWidth/45,10,15);
+    Scoretable * scores= new Scoretable(screenWidth/68);
+    QObject::connect(gameArea,SIGNAL(scoreChanged(int)),scores,SLOT(changeScore(int)));
+    QObject::connect(gameArea,SIGNAL(throwNextFigure(int,QColor)),scores,SLOT(setNextFigure(int,QColor)));
+    QWidget * wgt = new QWidget();
+    QHBoxLayout * lay = new QHBoxLayout();
+    lay->addWidget(gameArea);
+    lay->addWidget(scores);
+    wgt->setLayout(lay);
+    wgt->setFocusProxy(gameArea);
+    scores->setFocusProxy(gameArea);
+    gameArea->setFocus();
+    wgt->show();
+    gameArea->start();
     return a.exec();
 }
