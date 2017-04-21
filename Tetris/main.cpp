16130e34e57d598c595925c5a10d168e6544a5e0
Egor <youcankeepsilence@gmail.com>
2017-04-21 19:06:54 +0300

    before merge commit

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -10,15 +10,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     GLWidget w(30,10,15);
-//    Scoretable table(30,22,22);
-
     w.start();
     w.show();
-//    table.setNextFigure(Shape::Square, 1);
-//    table.show();
-
-
-//    emit w.scoreChanged(200);
-
     return a.exec();
 }
